9994a6e0760765d7ec59c2105e8ddb7f097b0b17
Will <will@thefixitgeek.com>
2015-09-10 14:44:01 -0600

    add collumns to 2 models

--- school/app/models/quiz.rb
+++ school/app/models/quiz.rb
@@ -1,3 +1,3 @@
 class Quiz < ActiveRecord::Base
-  belongs_to :course
+  belongs_to :courses
 end
---
+++ school/db/migrate/20150910203102_add_ids_to_courses.rb
@@ -0,0 +1,6 @@
+class AddIdsToCourses < ActiveRecord::Migration
+  def change 
+  	add_column :courses, :student_id, :integer
+  	add_column :courses, :teacher_id, :integer
+  end
+end
